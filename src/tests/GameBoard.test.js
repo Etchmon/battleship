e4c21e46b1dde0985925f18a5a59082d25441f6a
James Etchells <etch.rush@gmail.com>
2022-03-28 15:01:56 -0700

    Add another test for GameBoard

--- src/tests/GameBoard.test.js
+++ src/tests/GameBoard.test.js
@@ -10,3 +10,43 @@ test('Place ship', () => {
         hits: [],
     }]);
 });
+
+test('Place multiple ships', () => {
+    const board = new GameBoard();
+    board.placeShip('Cruiser', ['A1', 'A2', 'A3']);
+    board.placeShip('Carrier', ['A1', 'A2', 'A3', 'A4', 'A5']);
+    board.placeShip('Destroyer', ['A1', 'A2', 'A3', 'A4']);
+    board.placeShip('Submarine', ['A1', 'A2', 'A3']);
+    board.placeShip('Stealth', ['A1', 'A2']);
+    expect(board.shipArray).toEqual([
+        {
+            name: 'Cruiser',
+            position: ['A1', 'A2', 'A3'],
+            length: 3,
+            hits: [],
+        },
+        {
+            name: 'Carrier',
+            position: ['A1', 'A2', 'A3', 'A4', 'A5'],
+            length: 5,
+            hits: [],
+        },
+        {
+            name: 'Destroyer',
+            position: ['A1', 'A2', 'A3', 'A4'],
+            length: 4,
+            hits: [],
+        },
+        {
+            name: 'Submarine',
+            position: ['A1', 'A2', 'A3'],
+            length: 3,
+            hits: [],
+        },
+        {
+            name: 'Stealth',
+            position: ['A1', 'A2'],
+            length: 2,
+            hits: [],
+        }]);
+});
