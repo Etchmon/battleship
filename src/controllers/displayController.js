dc7619031c95d5e568d716f9cabc245f06cd6f2a
James Etchells <etch.rush@gmail.com>
2022-04-01 11:48:23 -0700

    Add controllers folder and files

---
+++ src/controllers/displayController.js
@@ -0,0 +1,21 @@
+import header from '../components/header';
+import main from '../components/main';
+import _ from 'lodash';
+
+const displayController = (() => {
+    const createDom = () => {
+        const content = document.createElement('section');
+
+        content.setAttribute('id', 'content');
+        content.innerHTML = _.join();
+
+        content.appendChild(header());
+        content.appendChild(main());
+
+        document.body.appendChild(content);
+    };
+
+    return { createDom };
+})();
+
+export default displayController;
