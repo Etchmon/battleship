89bc7f5438fd5143a23dea49f00b353d551ba6fd
James Etchells <etch.rush@gmail.com>
2022-04-04 12:57:11 -0700

    Add rotate method and handler during drop

--- src/controllers/gameController.js
+++ src/controllers/gameController.js
@@ -5,6 +5,7 @@ const gameController = (() => {
     let left;
     let right;
     let domShip;
+    let shipHtml;
     const initGame = () => {
         const player = new Player();
         const computer = new Computer();
@@ -24,6 +25,7 @@ const gameController = (() => {
     const drag = (ev) => {
         let length = ev.childNodes.length;
         let pos = ev.dataset.cell;
+        shipHtml = ev;
         right = (length - 1) - pos;
         left = (length - 1) - right;
         switch (ev.id) {
@@ -56,20 +58,82 @@ const gameController = (() => {
         const posArr = [pos];
         const shipyard = document.querySelector('.ship-yard');
         const fleetArr = Object.entries(currentPlayer.board.fleet);
+        const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
+        const classList = shipHtml.className;
+        console.log(classList.includes('rotate'));
+        if (classList.includes('rotate')) {
+            console.log('rotated')
+            const letterIndex = letters.indexOf(posLet);
+            // apply left to top, bottom to right variables
+            // similiar for loops, but instead of numbers use indexes in letters array
+            for (let i = left; i > 0; i--) {
+                if (left === 0) return;
+                const coord = letters[letterIndex - i];
+                if (coord == undefined) return;
+                posArr.push(coord + posNum);
+            };
 
-        for (let i = left; i > 0; i--) {
-            if (left === 0) return;
-            const coord = posNum - i;
-            if (coord < 0 || coord > 9) return;
-            posArr.push(posLet + coord);
+            for (let i = right; i > 0; i--) {
+                if (right === 0) return;
+                const coord = letters[Number(letterIndex) + i];
+                if (coord == undefined) return;
+                posArr.push(coord + posNum);
+            };
+        } else {
+            for (let i = left; i > 0; i--) {
+                if (left === 0) return;
+                const coord = posNum - i;
+                if (coord < 0 || coord > 9) return;
+                posArr.push(posLet + coord);
+            };
+
+            for (let i = right; i > 0; i--) {
+                if (right === 0) return;
+                const coord = Number(posNum) + i;
+                if (coord < 0 || coord > 9) return;
+                posArr.push(posLet + coord);
+            };
         };
 
-        for (let i = right; i > 0; i--) {
-            if (right === 0) return;
-            const coord = Number(posNum) + i;
-            if (coord < 0 || coord > 9) return;
-            posArr.push(posLet + coord);
-        };
+        // switch (classList) {
+        //     case (classList.includes('rotate') === true):
+        //         // find the position of posLet in letter array
+        //         console.log('rotated')
+        //         const letterIndex = letters.indexOf(posLet);
+        //         // apply left to top, bottom to right variables
+        //         // similiar for loops, but instead of numbers use indexes in letters array
+        //         for (let i = left; i > 0; i--) {
+        //             if (left === 0) return;
+        //             const coord = letters[letterIndex - i];
+        //             if (coord == undefined) return;
+        //             posArr.push(coord + posNum);
+        //         };
+
+        //         for (let i = right; i > 0; i--) {
+        //             if (right === 0) return;
+        //             const coord = letters[Number(posNum) + i];
+        //             if (coord == undefined) return;
+        //             posArr.push(coord + posNum);
+        //         };
+        //         break;
+        //     default:
+        //         for (let i = left; i > 0; i--) {
+        //             if (left === 0) return;
+        //             const coord = posNum - i;
+        //             if (coord < 0 || coord > 9) return;
+        //             posArr.push(posLet + coord);
+        //         };
+
+        //         for (let i = right; i > 0; i--) {
+        //             if (right === 0) return;
+        //             const coord = Number(posNum) + i;
+        //             if (coord < 0 || coord > 9) return;
+        //             posArr.push(posLet + coord);
+        //         };
+        //         break;
+        // };
+
+
         domShip.position = posArr;
         currentPlayer.board.placeShip(domShip);
 
