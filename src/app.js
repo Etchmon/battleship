1ef9417fec51804ffaf04bf3a864f3fddba0bedb
James Etchells <etch.rush@gmail.com>
2022-04-07 20:18:51 -0700

    Edit clean up comments

--- src/app.js
+++ src/app.js
@@ -4,11 +4,6 @@ import '../styles/style.css';
 import displayController from './controllers/displayController';
 import gameController from './controllers/gameController';
 // -------------------BattleShip----------------------
-// create a status div to display instruction messages based on the progress of the game
-// when ship is sunk display the name of the ship sunk in the status div
-// create a restart game function
-// restart game button that appears with play again message when all ships are sunk, alert the winner
-// work on transition animations
 
 const BattleShip = (() => {
 
